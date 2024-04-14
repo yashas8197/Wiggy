7c0c3caee0c0b66377df1a3decb69ccd025cc139
Yashas V <130334248+yashas8197@users.noreply.github.com>
2024-04-14 16:44:48 +0000

    feat: filter Top Restaurants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurantsList, setRestaurantsList] = useState(resList);
+
+  const filterTopRestaurants = () => {
+    const filterRestuarants = restaurantsList.filter((restaurant) => {
+      return restaurant?.info?.avgRating > 4.5;
+    });
+    setRestaurantsList(filterRestuarants);
+  };
   return (
     <div className="container my-4">
+      <div>
+        <button
+          className="btn btn-primary"
+          onClick={() => filterTopRestaurants()}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="row">
-        {resList.map((restaurant) => (
+        {restaurantsList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
