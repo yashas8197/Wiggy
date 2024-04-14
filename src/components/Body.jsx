cec27da31806625cb47ab3a6d90278a315644a31
Yashas V <130334248+yashas8197@users.noreply.github.com>
2024-04-14 18:11:40 +0000

    feat: search restaurant with search btn

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,20 @@ import { useState } from "react";
 
 const Body = () => {
   const [restaurantsList, setRestaurantsList] = useState(resList);
+  const [filteredRestuarant, setFilteredRestaurant] = useState(resList);
+  const [searchText, setSearchText] = useState("");
+
+  const onClickSearch = (e) => {
+    e.preventDefault();
+    const filteredRestaurants = restaurantsList.filter((restaurant) =>
+      restaurant.info.name.toLowerCase().includes(searchText.toLowerCase()),
+    );
+    if (filteredRestaurants !== "") {
+      setFilteredRestaurant(filteredRestaurants);
+    } else {
+      setFilteredRestaurant(resList);
+    }
+  };
 
   const filterTopRestaurants = () => {
     const filterRestuarants = restaurantsList.filter((restaurant) => {
@@ -13,16 +27,34 @@ const Body = () => {
   };
   return (
     <div className="container my-4">
-      <div>
+      <div className="d-flex justify-content-between">
         <button
           className="btn btn-primary"
           onClick={() => filterTopRestaurants()}
         >
           Top Rated Restaurants
         </button>
+        <form className="form-inline my-2 my-lg-0 d-flex">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={onClickSearch}
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
       </div>
+
       <div className="row">
-        {restaurantsList.map((restaurant) => (
+        {filteredRestuarant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
